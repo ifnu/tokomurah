3d3d16c087f5fbe0d06ca7d64852234fd86b7f6e
DESKTOP-IJULNUP\ifnub <ifnubima@gmail.com>
2022-11-30 22:03:52 +0700

    remove jdbc and spring data jdbc code

--- src/main/java/co/g2academy/tokomurah/db/tokomurah.sql
+++
@@ -1,9 +0,0 @@
-
-create table t_product (
-    id          integer auto_increment,
-    name        varchar(100),
-    description varchar(255),
-    price       integer not null,
-    stock       integer not null,
-    primary key(id)
-);
